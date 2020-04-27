3cd78c2eb577d4795130f6d6e2ebea0c1cefa6a7
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-27 17:47:52 -0400

    Haven't made that much progress. Began to write a little bit of the reducer and connect the Additional Features component.

--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -1,3 +1,29 @@
-export const carReducer = (state, action) => {
-  return state;
+export const initialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: [],
+  },
+  additionalFeatures: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 },
+  ],
+};
+
+export const carReducer = (state = initialState, action) => {
+  // console.log("state", state);
+
+  switch (action.type) {
+    case "Add_New_Feature":
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
 };
